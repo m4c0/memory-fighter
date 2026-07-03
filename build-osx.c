1d56f709371d9cb615425e80e24082501dab4240
Eduardo Costa <m4c0@users.noreply.github.com>
2026-07-03 22:56:12 +0100

    some boilerplate for building

---
+++ build-osx.c
@@ -0,0 +1,30 @@
+#define CFLAGS "-g", "-IVulkan-Headers/include"
+#define RES_PATH "boas.app/Contents/Resources"
+#include "build.h"
+
+#include <sys/stat.h>
+
+static int link_exe() {
+  RUN("clang", "-Wall",
+    "-framework", "AppKit",
+    "-framework", "AudioToolbox",
+    "-framework", "MetalKit",
+    "-o", "memory-fighter.app/Contents/MacOS/memory-fighter", 
+    OBJS, "vulkan-osx.o");
+  return 0;
+}
+
+int main(int argc, char ** argv) {
+  mkdir("memory-fighter.app", 0777);
+  mkdir("memory-fighter.app/Contents", 0777);
+  mkdir("memory-fighter.app/Contents/MacOS", 0777);
+  mkdir("memory-fighter.app/Contents/Resources", 0777);
+
+  RUN("cp", "libvulkan.dylib", "memory-fighter.app/Contents/MacOS/");
+
+  //CC("vulkan-osx.m", "vulkan-osx.o", CFLAGS);
+  if (compile_and_link_exe()) return 1;
+  if (shaders()) return 1;
+
+  return 0;
+}
